a26201cefd33fdc32b8e5cfc71df4fee7c09c2e0
JZU0 <ljzh1030@gmail.com>
2024-08-30 14:25:02 +0900

    init

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   };
 
   return (
-    <div className="App" onMouseUp={handleMouseUp}>
+    <div className="App" onMouseUp={handleMouseUp} onTouchEnd={handleSelection}>
       <header className="App-header">
         <p>
           Drag some text in this area and release the mouse button to see an
