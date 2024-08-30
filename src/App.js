4c79f6316f42a8cce0452a115bd4cae2e3349350
JZU0 <ljzh1030@gmail.com>
2024-08-30 14:46:26 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  const handleMouseUp = () => {
-    const selectedText = window.getSelection().toString().trim();
-    if (selectedText) {
-      alert(`You selected: "${selectedText}"`);
+  const [selectedText, setSelectedText] = useState('');
+
+  const handleTouchStart = () => {
+    // 터치가 시작될 때 선택된 텍스트를 초기화합니다.
+    setSelectedText('');
+  };
+
+  const handleTouchEnd = () => {
+    // 터치가 끝날 때 선택된 텍스트를 가져옵니다.
+    const text = window.getSelection().toString().trim();
+    if (text) {
+      setSelectedText(text);
+      alert(`You selected: "${text}"`);
     }
   };
 
   return (
-    <div className="App" onTouchEnd={handleMouseUp}>
+    <div
+      className="App"
+      onTouchStart={handleTouchStart}
+      onTouchEnd={handleTouchEnd}
+    >
       <header className="App-header">
         <p>
           Drag some text in this area and release the mouse button to see an
           alert. 드래그를 해보자 얍!
-        </p>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
         </p>
       </header>
     </div>
