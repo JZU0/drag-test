1cb5c4b696691d9533832e0d3fc9a78884148565
JZU0 <ljzh1030@gmail.com>
2024-08-30 14:21:24 +0900

    int

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const handleMouseUp = () => {
+    const selectedText = window.getSelection().toString().trim();
+    if (selectedText) {
+      alert(`You selected: "${selectedText}"`);
+    }
+  };
+
   return (
-    <div className="App">
+    <div className="App" onMouseUp={handleMouseUp}>
       <header className="App-header">
+        <p>
+          Drag some text in this area and release the mouse button to see an
+          alert. 드래그를 해보자 얍!
+        </p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
